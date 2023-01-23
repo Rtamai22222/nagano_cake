fdefb166608cc012e8001970d3202c182a2ccb72
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2023-01-23 01:20:31 +0000

    [Add] Deviseのコントローラとviewの作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :addresses, :cart_items, :orders
+  has_many :addresses
+  has_many :cart_items
+  has_many :orders
 end
