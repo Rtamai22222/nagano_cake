d27a49abb2ddf2a5183a50568ac0e4617581b1e1
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2023-01-25 01:06:51 +0000

    [Fnish] Adminのコントローラとviewの作成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  private
+
+    def after_sign_in_path_for(resource)
+      admin_root_path
+    end
+
+    def after_sign_out_path_for(resource)
+      new_admin_session_path
+    end
+
+
+    # def configure_permitted_parameters
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # end
+
+  # end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -15,9 +33,7 @@ class Admin::SessionsController < Devise::SessionsController
 
   # DELETE /resource/sign_out
   # def destroy
-  #   super
   # end
-
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  # devise_for :admins
-  # devise_for :customers
   devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
   }
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
   
+  namespace :admin do
+    root to: 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
+
 end
