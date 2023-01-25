63ecd51e55e05accbdd7aad51a4bc15a3b349dd6
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2023-01-25 10:46:15 +0000

    admin/genreページの作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// import "popper.js";
 import "bootstrap";
 import "../stylesheets/application.scss";
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
